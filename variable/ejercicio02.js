e25af359e4689592ce0b3c23a3ee9048949f07bf
agustin963 <230365@utxicotepec.edu.mx>
2024-09-19 14:27:50 -0600

    se agregaron diferentes tipos al archivo de ejercicio02

--- variable/ejercicio02.js
+++ variable/ejercicio02.js
@@ -60,3 +60,111 @@ console.log(`Tu saldo nuevo es de : ${saldo_cuenta}`);
 
 monto_transaccion=1500;
 console.log (`Tu abono de : ${monto_transaccion} ha sido recibido , tu nuevo saldo es de : ${saldo_cuenta+monto_transaccion}`)
+
+
+// string (cadena de carcateres)
+
+const alumno ="Jose Agustin Jimenez Castillo"
+let producto ;
+
+console.warn("-------Tipo de dato -String (Cadena de Carcacteres)");
+console.log(`El nombre del alumno es: ${alumno}, que es un tipo ed dato ${typeof (alumno)}`);
+
+// inicializamos el valor de la variable producto
+
+producto= "MONITOR 20\" FULL HD"
+console.log(`El nombre del PRODUCTO es: ${producto}, que es un tipo de dato ${typeof(producto)}`);
+
+//Manipulando los STRING
+console.log(`Mas adelante podremos transformar el contenido de los STRING usando los metodos y funciones especificas , como convertir su valor 
+    a mayusculas: ${alumno} => ${alumno.toUpperCase()}`);
+    console.log(`O en su defecto a minusculas: ${producto} => ${producto.toLocaleLowerCase()}`);
+
+// 5. BIGINT (Numero de Mayor Amplitud)
+
+let numeroGrande = 1234567890
+let numeroGrande2= 12345678901234567890
+let numeroGrande3= 123456789012345678901234567890
+let numeroGrande4= 1234567890123456789012345678901234567890
+
+console.log(`El primer experimento de un numero grande es : ${numeroGrande},que si es soportado por NUMBER y su tipo de dato es ${(numeroGrande)}`)
+console.log(`El segundo experimento de un numero grande es : ${numeroGrande2},que si es soportado por NUMBER y su tipo de dato es ${(numeroGrande2)}`)
+console.log(`El tercer experimento de un numero grande es : ${numeroGrande3},que no es soportado por NUMBER perdiendo presicion y su tipo de dato es ${(numeroGrande3)}`)
+console.log(`El cuarto experimento de un numero grande es : ${numeroGrande4},que no es soportado por NUMBER perdiendo presicion y su tipo de dato es ${(numeroGrande4)}`)
+
+numeroGrande3 = BigInt("123456789012345678901234567890")
+console.log(`El quinto experimento de un numero grande es : ${numeroGrande3},que ya fue convertido a BIGINT  y su tipo de dato es ${(numeroGrande3)}`)
+
+numeroGrande4 = BigInt("1234567890123456789012345678901234567890")
+console.log(`El SEXTO experimento de un numero grande es : ${numeroGrande4},que ya fue convertido a BIGINT  y su tipo de dato es ${(numeroGrande4)}`)
+
+// se pueden realizar operaciones con bigint
+const numero = 129 ;
+console.log(`Intentado realizar la suma: numero + numeroGrande3 , el resultado es: ${BigInt(numero) + numeroGrande3}`)
+
+// 6. SYMBOL (simbolo)
+console.warn("-------Tipo de dato SYMBOL (Simbolo o Unico)")
+const numero1 = 5;
+const numero2 = 5.0;
+const numero3 = "5";
+const numero4 = "5.0";
+const numero5 = Symbol(5)
+const numero6 = Symbol(5)
+const numero7 = Symbol(5.0)
+const numero8 = Symbol("5")
+const numero9 = Symbol("5.0")
+
+//pruebas comparativas
+console.log("¿Es 5 = \"5\"?")
+if (numero1 == numero2) {
+    console.log("Se comparo numero1 con numero2, determina que tiene el mismo valor.")
+} else {
+    console.log("Se comparo numero1 con numero2, determinado que No tienen el mismo valor ")
+
+}
+
+console.log("¿Es 5 = \"5.0\"?")
+
+if (numero1 === numero3) {
+    console.log("Se comparo numero1 con numero4, determina que tiene el mismo valor.")
+} else {
+    console.log("Se comparo numero1 con numero4, determinado que No tienen el mismo valor ")
+
+}
+
+
+
+console.log("¿Es 5 = \"5.0\"?")
+
+if (numero1 === numero4) {
+    console.log("Se comparo numero1 con numero5, determina que tiene el mismo valor.")
+} else {
+    console.log("Se comparo numero1 con numero5, determinado que No tienen el mismo valor ")
+
+}
+
+
+
+console.log("¿Es 5 = \"5.0\"?")
+
+if (numero1 === numero5) {
+    console.log("Se comparo numero1 con numero2, determina que tiene el mismo valor.")
+} else {
+    console.log("Se comparo numero1 con numero2, determinado que No tienen el mismo valor ")
+
+}
+
+
+
+
+console.log(`¿Es 5 = Symbol(5)? , antes de comnparamos analizamos que ripo de dato son: siendo numero1 del tipo ${typeof(numero1)} y numero5 del tipo ${typeof(numero5)}`)
+
+if (numero1 === numero5) {
+    console.log("Se comparo numero1 con numero5, determina que tiene el mismo valor.")
+} else {
+    console.log("Se comparo numero1 con numero5, determinado que  tienen el mismo valor pero no el mismo tipo de dato ")
+
+}
+
+
+
