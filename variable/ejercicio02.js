b9f01b7097fc2ae529b634df115e6845b856588d
agustin963 <230365@utxicotepec.edu.mx>
2024-09-22 22:51:41 -0600

    Practica 04
    Se agrago el tipo de dato funcion

--- variable/ejercicio02.js
+++ variable/ejercicio02.js
@@ -226,6 +226,16 @@ console.log(
     "las variables "
 )
 
+// 8. FUNCTION (Funciones)
+console.warn("--- Tipo de Dato FUNCTION (Función)")
 
+// Declaramos una funcion que nos permita recibir un paramento en este caso el nombre de la persona a saludar, y le enviamos un saludo, esta función la asignamos a una constante.
+const saludar =  function(nombre){return `Hola, ${nombre}!`}
+
+// Invocamos a la función declarada
+console.log(saludar('Marco'));
+
+//Y que tipo de dato tiene esta constante
+console.log(`El tipo de dato de la constante saludar es: ${typeof(saludar)}`)
 
 
